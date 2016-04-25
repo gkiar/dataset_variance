71ac55ba2fb95de7587b241090249a82162c59b7
gkiar <gkiar07@gmail.com>
2016-04-25 14:29:48 -0400

    added multiplots for other invariants

--- multiplot.py
+++ multiplot.py
@@ -99,28 +99,149 @@ class hist_plot():
         return set([item for subitem in 
                     [(i, N//i) for i in range(1, int(N**0.5) + 1) if N % i == 0 and i > 1]
                     for item in subitem])
+
+
+class feature_plot():
+
+    def __init__(self, data, names, figtitle, figname=None, plotm='bar', ylims = None):
+        """
+        Plots multiple populations of histograms in the same figure.
+        Expects:
+            fnames: list of filenames, each of which is a pickle dictionary containing
+                    pdfs and x values over which they were evaluated
+            names: list of length equal to filenames, which is the coloquial name of the
+                   dataset we want in the figure title
+        """
+        self.data = data
+        self.names = names
+        self.figname = figname
+        self.figtitle = figtitle
+        self.ylims = ylims
+        if plotm == 'bar' :
+            self.bar_plot()
+        elif plotm == 'scatter' :
+            self.scatter_plot()
+        else :
+            self.hist_plot()
+
+    def bar_plot(self):
+        """
+        Plots series of histograms in a single figure
+        """
         
-#     def plotting(self, xl, xh):
-#         """
-#         Define interactive plot function for altering pdf range
-#         """
-#         # eps = np.min([np.min(self.tpdfs[key]) for key in self.tpdfs.keys() if np.min(self.tpdfs[key]) > 0])
-#         #print eps
-#         eps = 1e-9/2
-#         fig = plt.figure(figsize=(8, 6))
-#         ax = plt.subplot(111)
-#         plt.hold(True)
-#         for key in self.tpdfs.keys():
-#             if not self.color:
-#                 plt.plot(self.txs[xl:xh]+1, self.tpdfs[key][xl:xh]+eps, alpha=0.4,
-#                          color='#888888')
-#             else:
-#                 plt.plot(self.txs[xl:xh]+1, self.tpdfs[key][xl:xh]+eps, alpha=0.4)
-#             plt.title(self.name + ' histograms')
-#         plt.xlabel('Voxel Intensity')
-#         plt.ylabel('Probability Density')
-#         ax.set_xscale('log')
-#         ax.set_yscale('log')
-#         plt.show()
-#         print "X-Min:", self.txs[xl]
-#         print "X-Max:", self.txs[xh]
+        """
+        Setup plot shape, etc.
+        """
+        eps = 1e-9/2
+        N = len(self.names)
+        ds = list()
+        count = 0
+        while len(ds) == 0:
+            ds = list(self.factors(N+count))
+            count += 1
+            print ds
+        if len(ds) == 1:
+            ds = list((ds[0], ds[0]))
+        fig = plt.figure(figsize=(4*ds[-1], 4*ds[0]))
+        bl = ds[-1]*(ds[0]-1)
+
+        """
+        Actually plot things
+        """
+        for idx, set in enumerate(self.data.keys()):
+            ax = plt.subplot(ds[0], ds[-1], idx+1)
+            plt.hold(True)
+            plt.bar(range(len(self.data[set])),self.data[set].values(), alpha=0.7, color='#888888')
+            plt.title(set, y = 1.04)
+            if idx == bl:
+                plt.ylabel('Count')
+                plt.xlabel('Graph')
+            plt.xlim((0, len(self.data[set].keys())))
+            if self.ylims is not None:
+                plt.ylim(self.ylims)
+
+            plt.tight_layout()
+
+        if self.figname is not None:
+            plt.savefig(self.figname)
+        plt.show()
+
+    def scatter_plot(self):
+        """
+        Plots series of histograms in a single figure
+        """
+        
+        """
+        Setup plot shape, etc.
+        """
+        N = len(self.names)
+        fig = plt.figure(figsize=(1.2*N, 1*N))
+        
+        """
+        Actually plot things
+        """
+        ax = plt.subplot(1,1,1)
+        plt.hold(True)
+        for idx, set in enumerate(self.data.keys()):
+            plt.scatter([idx] * len(self.data[set].values()),self.data[set].values(), alpha=0.1, color='#000000')
+
+        plt.title(self.figtitle, y = 1.04)
+        plt.ylabel('Count')
+        ax.set_xticks(np.arange(len(self.names)))
+        ax.set_xticklabels(self.names, rotation=40)
+        plt.xlabel('Dataset')
+        plt.xlim([-0.5, len(self.names)-0.5])
+        if self.ylims is not None:
+            plt.ylim(self.ylims)
+
+        plt.tight_layout()
+
+        if self.figname is not None:
+            plt.savefig(self.figname)
+        plt.show()
+
+
+    def hist_plot(self):
+        """
+        Plots series of histograms in a single figure
+        """
+        eps = 1e-9/2
+        N = len(self.fnames)
+
+        ds = list()
+        count = 0
+        while len(ds) == 0:
+            ds = list(self.factors(N+count))
+            count += 1
+            print ds
+        
+        if len(ds) == 1:
+            ds = list((ds[0], ds[0]))
+        fig = plt.figure(figsize=(4*ds[-1], 4*ds[0]))
+        bl = ds[-1]*(ds[0]-1)
+        for idx, key in enumerate(self.data.keys()):
+            ax = plt.subplot(ds[0], ds[-1], idx+1)
+            plt.hold(True)
+            xs = self.data[key]['xs']
+            pdfs = self.data[key]['pdfs']
+            for key2 in pdfs:
+                if not self.color:
+                    plt.plot(xs+1, pdfs[key2]+eps, alpha=0.07, color='#000000')
+                else:
+                    plt.plot(xs+1, pdfs[key2]+eps, alpha=0.4)
+            plt.title(key)
+            if idx == bl:
+                plt.xlabel('Voxel Intensity')
+                plt.ylabel('Probability Density')
+            ax.set_xscale('log')
+            ax.set_yscale('log')
+        plt.tight_layout()
+        if self.figname is not None:
+            plt.savefig(self.figname)
+        plt.show()
+        
+        
+    def factors(self, N): 
+        return set([item for subitem in 
+                    [(i, N//i) for i in range(1, int(N**0.5) + 1) if N % i == 0 and i > 1]
+                    for item in subitem])
